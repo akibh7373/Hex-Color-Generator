0611798fea669f275eb5b4f7c9533d25df8ec936
Akib Hossain <44940208+akib737@users.noreply.github.com>
2022-07-07 00:29:22 +0600

    Initial commit

---
+++ asset/app.js
@@ -0,0 +1,51 @@
+// DOM Selection
+const color = document.querySelector(".color");
+const btn = document.querySelector(".btn");
+
+// Add Event Listener
+btn.addEventListener("click", function () {
+  // To left color
+  // Take colors into an array
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const hex2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const hex3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const hex4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const hex5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const hex6 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let hexaColor = "#";
+  let hexaColor2 = "";
+  let hexaColor3 = "";
+  let hexaColor4 = "";
+  let hexaColor5 = "";
+  let hexaColor6 = "";
+  const ranNumb = Math.floor(Math.random() * hex.length);
+  const ranNumb2 = Math.floor(Math.random() * hex2.length);
+  const ranNumb3 = Math.floor(Math.random() * hex3.length);
+  const ranNumb4 = Math.floor(Math.random() * hex4.length);
+  const ranNumb5 = Math.floor(Math.random() * hex5.length);
+  const ranNumb6 = Math.floor(Math.random() * hex6.length);
+  
+  for (let i = 0; i < 1; i++) {
+    hexaColor += hex[ranNumb];
+  }
+   for (let i = 0; i < 1; i++) {
+    hexaColor2 += hex2[ranNumb2];
+  }
+  for (let i = 0; i < 1; i++) {
+    hexaColor3 += hex2[ranNumb3];
+  }
+  for (let i = 0; i < 1; i++) {
+    hexaColor4 += hex[ranNumb4];
+  }
+   for (let i = 0; i < 1; i++) {
+    hexaColor5 += hex2[ranNumb5];
+  }
+  for (let i = 0; i < 1; i++) {
+    hexaColor6 += hex2[ranNumb6];
+  }
+  const allHexNumb = hexaColor+hexaColor2+hexaColor3+hexaColor4+hexaColor5+hexaColor6;
+
+  document.body.style.backgroundColor = allHexNumb;
+  color.textContent = allHexNumb;
+  color.style.backgroundColor = allHexNumb;
+});
